4f3fcbc12b862d1660180923ab609d16776d7d40
Ashis Kumar Naik <ashishami2002@gmail.com>
2024-10-30 23:54:02 +0530

    tested openai client in rust
    Signed-off-by: Ashis Kumar Naik <ashishami2002@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,21 @@
 pub mod openai_call;
 
-
+async fn main() {
+    let response= openai_call::test_embedding_from_openai().await;
+    println!("{0}", response);
+}
 
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[tokio::test]
+    async fn test_embedding() {
 
+        main().await;
+
+    }
 
 
 }
--- src/openai_call/mod.rs
+++ src/openai_call/mod.rs
@@ -1,1 +1,24 @@
-pub fn test_embedding_from_openai() {}
+use async_openai::{Client, types::{CreateCompletionRequestArgs}};
+
+pub async fn test_embedding_from_openai() -> String {
+
+    let client = Client::new();
+
+
+    let request = CreateCompletionRequestArgs::default()
+        .model("gpt-3.5-turbo-instruct")
+        .prompt("Tell me the recipe of alfredo pasta")
+        .max_tokens(40_u32)
+        .build()
+        .unwrap();
+
+
+    let response = client
+        .completions()      // Get the API "group" (completions, images, etc.) from the client
+        .create(request)    // Make the API call in that "group"
+        .await
+        .unwrap();
+
+    let response_str = response.choices.first().unwrap().text.clone();
+    response_str
+}
